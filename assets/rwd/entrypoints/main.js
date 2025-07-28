7dbdd4f4389b2acb0f26b7706d1d6149764d874f
jaroslawmajcher <jarek@majcher.me>
2025-07-28 23:22:01 +0200

    feat: templates, styles, js

--- assets/rwd/entrypoints/main.js
+++ assets/rwd/entrypoints/main.js
@@ -1,10 +1,7 @@
 // JS
-import 'mburger-webcomponent/dist/mburger/index'
-import '../js/menu'
 
 // CSS
 import '../scss/reset.scss'
-import 'mmenu-js/dist/mmenu.css'
 import '../scss/elements.scss'
 import '../scss/layout.scss'
 import '../scss/navigation-desktop.scss'
